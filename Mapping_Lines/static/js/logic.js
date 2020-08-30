a7eea5baab38a3e634f3f3d7e3fa40393056af13
Tim Smith <tgs584@utexas.edu>
2020-08-30 17:43:49 -0500

    Adding Line Data

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,13 +2,13 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Create the map object with a center and zoom level. MANUALLY
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
@@ -42,3 +42,21 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
 });
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
+
+
+//SKILLDRILL
+
+//Skipped. I understand how. All I would do is change the citities in a variable array to match those of the drill and remembert to add in my city
